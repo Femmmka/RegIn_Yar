23d1b5b1b454a3b5b3c4b66c24dac90626efcbb7
Ефим <fimakrut@mail.ru>
2024-05-15 08:43:34 +0500

    Разработанная страница Login. Ярыгин

--- RegIn_Yar/MainWindow.xaml.cs
+++ RegIn_Yar/MainWindow.xaml.cs
@@ -6,34 +6,50 @@ using System.Windows.Media.Animation;
 
 namespace RegIn_Yar
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+
         public static MainWindow mainWindow;
+
+
+        public User UserLogIn = new User();
         public MainWindow()
         {
             InitializeComponent();
             mainWindow = this;
+
             OpenPage(new Pages.Login());
         }
+
         public void OpenPage(Page page)
         {
+
             DoubleAnimation StartAnimation = new DoubleAnimation();
+
             StartAnimation.From = 1;
+
             StartAnimation.To = 0;
+
             StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+
             StartAnimation.Completed += delegate
             {
+
                 frame.Navigate(page);
+
                 DoubleAnimation EndAnimation = new DoubleAnimation();
+
                 EndAnimation.From = 0;
+
                 EndAnimation.To = 1;
+
                 EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
-                frame.BeginAnimation(Frame.OpacityMaskProperty, StartAnimation);
+
+                frame.BeginAnimation(Frame.OpacityProperty, EndAnimation);
             };
-            frame.BeginAnimation(Frame.OpacityMaskProperty, StartAnimation);
+
+            frame.BeginAnimation(Frame.OpacityProperty, StartAnimation);
         }
     }
 }
